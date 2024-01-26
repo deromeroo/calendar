9ec0a4a4dc5b38a9764fb07d60f430879f0abb7b
Diego Romero - deromeroo <deromero707.com>
2024-01-26 12:13:18 -0400

    :construction: [ADD] Adding TailwindCSS and  Nice Background

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,7 @@
+export const CalendarApp = () => {
+  return (
+    <main className="absolute">
+      <h1>HEllo</h1>
+    </main>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/800.css'
+
+import './styles.css'
+
+import { CalendarApp } from './CalendarApp'
+import { Background } from './components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Background />
+    <CalendarApp />
+  </React.StrictMode>
 )
